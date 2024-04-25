58988ec01029c530553764c6b61487a10fd86aeb
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-04-25 15:06:32 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    for (let index = 0; index < 2; index++) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 10)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    maqueen.motorStop(maqueen.Motors.All)
 })
